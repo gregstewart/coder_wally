c4a20f41033008e4df3246e39bd2559d89ed4141
Greg Stewart <greg.stewart@gmail.com>
2015-01-23 17:03:38 +0000

    Added c9 folder to ignore list

--- lib/coder_wally.rb
+++ lib/coder_wally.rb
@@ -1,7 +1,26 @@
 require "coder_wally/version"
 
+require "open-uri"
+require "json"
+
 # All code in the gem is namespaced under this module.
 module CoderWally
   # The URL of API we'll use.
-  Url = "https://coderwall.com/{username}.json"
+  Url = "https://coderwall.com/%s.json"
+  
+  class Badge 
+    attr_reader :name, :badge, :description, :created
+    
+    def initialize(hashed_badge)
+      @name, @badge, @description, @created = hashed_badge.values
+    end
+  end
+  
+  def CoderWally.get_badges_for(username)
+      url = URI.parse(Url % username)
+      puts url
+      response = JSON.load(open(url))      
+
+      response["badges"].map { |badge| Badge.new(badge) }
+  end
 end
--- test/coder_wally_test.rb
+++ test/coder_wally_test.rb
@@ -1,9 +1,30 @@
 require "minitest/autorun"
 
 require "coder_wally"
+require "webmock/minitest"
 
 describe "Coder Wally" do
   it "has a default api url" do
-    "https://coderwall.com/{username}.json".must_equal CoderWally::Url
+    "https://coderwall.com/%s.json".must_equal CoderWally::Url
+  end
+  
+  describe "user badges" do
+      describe "valid user" do
+        before do
+            success_response = open(File.expand_path(File.dirname(__FILE__) + "/./fixtures/200.json")).read
+            stub_request(:get, "https://coderwall.com/me.json").
+              with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+              to_return(:status => 200, :body => success_response, :headers => {})
+        end
+        
+        it "returns a hash of badges" do
+          badges = CoderWally.get_badges_for('me')
+          
+          badges.count.must_equal 11
+          badges.first.name.must_equal "Lab"
+        end
+        
+      end
+      
   end
 end
