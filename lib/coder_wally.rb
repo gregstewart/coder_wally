05688727751e226d298f34795a265706e0c7176c
Greg Stewart <greg.stewart@gmail.com>
2015-01-23 17:27:00 +0000

    Basic fetching added and simple validation done

--- lib/coder_wally.rb
+++ lib/coder_wally.rb
@@ -12,15 +12,18 @@ module CoderWally
     attr_reader :name, :badge, :description, :created
     
     def initialize(hashed_badge)
-      @name, @badge, @description, @created = hashed_badge.values
+      @name = hashed_badge.fetch("name")
+      @badge = hashed_badge.fetch("badge")
+      @description = hashed_badge.fetch("description") 
+      @created = hashed_badge.fetch("created")
     end
   end
   
-  def CoderWally.get_badges_for(username)
-      url = URI.parse(Url % username)
-      puts url
-      response = JSON.load(open(url))      
+  def CoderWally.get_badges_for username
+    raise(ArgumentError, "Plesae provide a username") if username.empty?
+    url = URI.parse(Url % username)
+    response = JSON.load(open(url))      
 
-      response["badges"].map { |badge| Badge.new(badge) }
+    response["badges"].map { |badge| Badge.new(badge) }
   end
 end
--- test/coder_wally_test.rb
+++ test/coder_wally_test.rb
@@ -22,9 +22,28 @@ describe "Coder Wally" do
           
           badges.count.must_equal 11
           badges.first.name.must_equal "Lab"
+          badges.first.badge.must_equal "https://d3levm2kxut31z.cloudfront.net/assets/badges/labrador-1eb0fc532826e5a7c5487855f9880627.png"
+          badges.first.description.must_equal "Have at least one original repo where C# is the dominant language"
+          badges.first.created.must_equal "2014-07-10T23:36:47Z"
+        end
+      end
+      describe "invalid user" do
+        it "throws an exception when no user is passed in" do
+          err = ->{ CoderWally.get_badges_for }.must_raise ArgumentError
+          err.message.must_match /wrong number/
         end
         
+        it "throws an exception when empty string is passed in" do
+          err = ->{ CoderWally.get_badges_for "" }.must_raise ArgumentError
+          err.message.must_match /Plesae provide a username/
+        end
+        
+        # before do
+        #   not_found_response = open(File.expand_path(File.dirname(__FILE__) + "/./fixtures/404.json")).read
+        #     stub_request(:get, "https://coderwall.com/me.json").
+        #       with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+        #       to_return(:status => 404, :body => not_found_response, :headers => {})
+        # end
       end
-      
   end
 end
