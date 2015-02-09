f4f42a956c681c31aa6feaadf4c8c0db981e5b67
Greg Stewart <greg.stewart@gmail.com>
2015-02-09 17:51:58 +0000

    Fixed code smells in api around parsing errors (duplicate calls and feature envy)

---
+++ lib/coder_wally/status_code_from_error.rb
@@ -0,0 +1,26 @@
+# All code in the gem is namespaced under this module.
+module CoderWally
+    # return status code from error object
+    class StatusCodeFromError
+        # Initialise object
+        def initialize(error)
+            @error = error 
+        end
+        # Get the status code
+        def get_status_code
+            @error.io.status[0] if has_error && has_io && has_status_code
+        end
+        # Is there an error object?
+        def has_error
+            true if @error
+        end
+        # Is there an io object?
+        def has_io
+            true if @error.io 
+        end
+        # Is there a status?
+        def has_status_code
+            true if @error.io.status
+        end
+    end
+end
